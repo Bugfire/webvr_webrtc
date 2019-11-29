794174e2ea964a2b442add758423b287605e4bdf
Bugfire <bugfire@users.noreply.github.com>
2019-11-29 18:21:03 +0900

    Refactoring

--- pi/motor_api.py
+++ pi/motor_api.py
@@ -12,46 +12,50 @@ PORT = 5000
 i2c = smbus.SMBus(1)
 
 ch1 = DRV8830(i2c, 0x60)
-ch1.clear_fault()
-ch1.reset()
-
 ch2 = DRV8830(i2c, 0x65)
-ch2.clear_fault()
-ch2.reset()
+
+def reset_motors():
+    ch1.clear_fault()
+    ch1.reset()
+    ch2.clear_fault()
+    ch2.reset()
+
+def signal_handler(signum, frame):
+    print('Terminating...')
+    reset_motors()
+    sys.exit(1)
 
 class MotorAPIHandler(BaseHTTPRequestHandler):
-    def _set_handlers(self):
-        self.send_response(200)
+    def _set_handlers(self, status):
+        self.send_response(status)
         self.send_header('Content-Type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
     def do_GET(self):
-        self._set_handlers()
         if self.path == '/cmd/forward':
             ch1.reset()
             ch2.forward()
-        if self.path == '/cmd/backward':
+        elif self.path == '/cmd/backward':
             ch1.reset()
             ch2.backward()
-        if self.path == '/cmd/left':
+        elif self.path == '/cmd/left':
             ch2.reset()
             ch1.forward()
-        if self.path == '/cmd/right':
+        elif self.path == '/cmd/right':
             ch2.reset()
             ch1.backward()
-        if self.path == '/cmd/stop':
+        elif self.path == '/cmd/stop':
             ch1.reset()
             ch2.reset()
-        self.wfile.write(json.dumps({"status": "ok"}).encode('utf-8'))
+        else:
+            self._set_handlers(400)
+            self.wfile.write(json.dumps({"status": "error"}).encode("utf-8"))
+            return
+        self._set_handlers(200)
+        self.wfile.write(json.dumps({"status": "ok"}).encode("utf-8"))
 
-def signal_handler(signum, frame):
-    print('Terminating...')
-    ch1.clear_fault()
-    ch1.reset()
-    ch2.clear_fault()
-    ch2.reset()
-    sys.exit(1)
+reset_motors()
 
 signal.signal(signal.SIGINT, signal_handler)
 
